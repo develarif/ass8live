2002f9bc7dcee94432f19a43a3c03c65a7bd02b3
sikdermdariff <sikdermohammadarif@gmail.com>
2021-09-26 22:46:35 +0600

    Last commit

---
+++ public/README.md
@@ -0,0 +1,3 @@
+# Simple Salery Counting apps usign react js
+
+Click here to see live website [Go to the live site](https://google.com).
---
+++ public/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+
+function App() {
+	return (
+		<div className='wrapper'>
+			<Header></Header>
+			<Content></Content>
+		</div>
+	);
+}
+
+export default App;
